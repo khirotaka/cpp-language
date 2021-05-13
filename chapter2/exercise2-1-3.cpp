f20d4db63e3b7332cb359bb33168e0147a1ac615
Hirotaka Kawashima <k.hirotaka@me.com>
2021-05-13 23:02:22 +0900

    exercise 2

---
+++ chapter2/exercise2-1-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+struct S {
+    int x;
+    int y;
+};
+
+union U {
+    S s;
+};
+
+int main() {
+    U u = {};
+    u.s.x = 10;
+    u.s.y = 20;
+
+    std::cout << u.s.x << std::endl;
+}
+
